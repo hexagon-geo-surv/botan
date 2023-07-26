cb3888fccc4dbb15f11efd7f4852d07ecfa0fd5d
Jack Lloyd <jack@randombit.net>
2023-07-26 18:29:53 -0400

    Fix clang-tidy readability-avoid-const-params-in-decls in headers

--- src/lib/pubkey/dilithium/dilithium_aes/dilithium_aes.h
+++ src/lib/pubkey/dilithium/dilithium_aes/dilithium_aes.h
@@ -23,7 +23,7 @@ namespace Botan {
 class Dilithium_AES_Symmetric_Primitives : public Dilithium_Symmetric_Primitives {
    public:
       // AES mode always uses AES-256, regardless of the XofType
-      std::unique_ptr<StreamCipher> XOF(const XofType /* type */,
+      std::unique_ptr<StreamCipher> XOF(XofType /* type */,
                                         std::span<const uint8_t> seed,
                                         uint16_t nonce) const final {
          auto cipher = StreamCipher::create_or_throw("CTR(AES-256)");
