4c58de854d20736a9d3c9b0a1037bfebf6e15bb1
Jack Lloyd <jack@randombit.net>
2024-12-02 07:45:54 -0500

    WIP on generic pcurves for application curves/etc

--- src/lib/pubkey/ec_group/ec_inner_data.cpp
+++ src/lib/pubkey/ec_group/ec_inner_data.cpp
@@ -61,6 +61,13 @@ EC_Group_Data::EC_Group_Data(const BigInt& p,
       }
    }
 
+   // Try a generic pcurves instance
+   if(!m_pcurve) {
+      m_pcurve = PCurve::PrimeOrderCurve::from_params(p, a, b, g_x, g_y, order);
+      // possibly still null here, if parameters unsuitable or if the
+      // pcurves_generic module wasn't included in the build
+   }
+
 #if defined(BOTAN_HAS_LEGACY_EC_POINT)
    secure_vector<word> ws;
    m_a_r = m_monty.mul(a, m_monty.R2(), ws);
